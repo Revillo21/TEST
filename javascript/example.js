3dc4032eaeaecbb3e8535876526ae2e32d8b3c75
Revillo21 <royceolliver@hotmail.com>
2015-10-28 21:07:18 -0700

    anything

---
+++ javascript/example.js
@@ -0,0 +1,21 @@
+var someNumber =12; 
+var anotherNumber = someNumber + 4; 
+var name = 'charlie';
+var aLocation = "university of washing";
+var quote = 'a great man once said "always bla blah"'
+console.log(anotherNumber);
+console.log(someNumber);
+cosole.log(quote);
+
+var firstFavFood = 'pizza';
+var secondFavFood = 'ice cream';
+
+var favoriteFoods = ['pizza', 'ice cream', 'burgers'];
+console.log(favoriteFoods);
+
+console.log(favoriteFoods[0]);
+console.log(favoriteFoods[1]);
+
+var thirdFav = favoriteFoods[2];
+console.log(thirdFav);
+
